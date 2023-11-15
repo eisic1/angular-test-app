e519b5bb29c15f853bb8d08e1a268fe121551ffc
esmir <esmirisic@gmail.com>
2023-11-15 21:00:08 +0100

    deifferent selectors

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // Different selectors
+  // selector: '[app-nav]'
+  // selector: '.app-nav'
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-success"><p>This website uses cookies to provide better user experience.</p></div>`,
+  template: `<div class="alert alert-danger"><p>This website uses cookies to provide better user experience.</p></div>`,
   //styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent {
