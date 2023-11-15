6fa4072e6037b3f9a6ff75e073b2300db4637d57
esmir <esmirisic@gmail.com>
2023-11-15 23:52:00 +0100

    property binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   slogan: string = 'Your one stop shop fo everything.';
+  source: string = '../../assets/shopping.png';
 }
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-danger"><p>This website uses cookies to provide better user experience.</p></div>`,
+  template: `<div class="alert alert-danger" [hidden]="displayNotification">
+              <p>This website uses cookies to provide better user experience.</p>
+            </div>`,
   //styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent {
-
+  displayNotification: boolean = false;
 }
