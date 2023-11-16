1c491ca7fed0af464be914d0570852aa0727492f
esmir <esmirisic@gmail.com>
2023-11-16 23:43:27 +0100

    ngFor directive

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  products = [
+    { id: 1, name: 'Smart Watch', price: '100', color: 'Black', available: 'Available' },
+    { id: 2, name: 'Smart Watch', price: '100', color: 'Black', available: 'Available' },
+    { id: 3, name: 'Smart Watch', price: '100', color: 'Black', available: 'Available' },
+    { id: 4, name: 'Smart Watch', price: '100', color: 'Black', available: 'Available' },
+    { id: 5, name: 'Smart Watch', price: '100', color: 'Black', available: 'Available' },
+    { id: 6, name: 'Smart Watch', price: '100', color: 'Black', available: 'Available' },
+  ];
+}
